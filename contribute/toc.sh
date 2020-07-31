0b974696cd56771c244b301858c4fc78c589545d
pascal <pascal.morin@codeenigma.com>
2020-07-31 12:45:00 +0200

    TOC amends

--- contribute/toc.sh
+++ contribute/toc.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # shellcheck disable=SC2094
 # shellcheck disable=SC2129
-
+IFS=$(printf '\n\t')
 set -e
-
 OWN_DIR=$(dirname "$0")
 cd "$OWN_DIR" || exit 1
 OWN_DIR=$(git rev-parse --show-toplevel)
@@ -72,6 +71,51 @@ generate_role_variables(){
   fi
 }
 
+generate_roles_toc(){
+  TMP_SIDEBAR=$(mktemp)
+  WRITE="true"
+  while read -r LINE; do
+    case $LINE in
+    "  - [Roles](roles)")
+      echo "$LINE" >> "$TMP_SIDEBAR"
+      parse_roles_toc roles 2
+      WRITE="false"
+    ;;
+    "  -"*)
+      WRITE="true"
+      echo "$LINE" >> "$TMP_SIDEBAR"
+    ;;
+    *)
+    if [ "$WRITE" = "true" ]; then
+      echo "$LINE" >> "$TMP_SIDEBAR"
+    fi
+    ;;
+    esac
+  done < "$OWN_DIR/docs/_Sidebar.md"
+  mv "$TMP_SIDEBAR" "$OWN_DIR/docs/_Sidebar.md"
+}
+
+parse_roles_toc(){
+  ROLES=$(find "$OWN_DIR/$1" -mindepth "$2" -maxdepth "$2" -name "README.md" | sort)
+  for ROLE in $ROLES; do
+    WRITE="true"
+    INDENT=$(printf %$(($2 * 2))s)
+    RELATIVE=$(realpath --relative-to="$OWN_DIR" "$(dirname "$ROLE")")
+    while read -r LINE; do
+      case $LINE in
+      "# "*)
+        if [ "$WRITE" = "true" ]; then
+          TITLE=$(echo "$LINE" | cut -c 3-)
+          echo "$INDENT"" - [$TITLE]($RELATIVE)" >> "$TMP_SIDEBAR"
+          WRITE="false"
+        fi
+      ;;
+      esac
+    done < "$ROLE"
+  parse_roles_toc "$RELATIVE" $(($2 + 1))
+  done
+}
+
 rm -rf "$OWN_DIR/docs/roles"
 ROLE_PAGES=$(find "$OWN_DIR/roles" -name "README.md")
 for ROLE_PAGE in $ROLE_PAGES; do
@@ -80,6 +124,8 @@ done
 for ROLE_PAGE in $ROLE_PAGES; do
   cp_role_page "$ROLE_PAGE"
 done
+generate_roles_toc
+
 
 cp_single_page install
 cp_single_page contribute
