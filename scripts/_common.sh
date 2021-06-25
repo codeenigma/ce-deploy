cae4dde77ac7c44ce820b1089ccdc4d781d5ba1e
Greg Harvey <greg.harvey@gmail.com>
2021-06-25 14:38:45 +0200

    Python3 path change pr devel (#27)
    * Adding GitHub Actions CI to the project. (#24)
    
    * GitHub actions linting issues. (#25)
    
    * Adding GitHub Actions CI to the project.
    
    * Fixing linting issues.
    
    * Adding GitHub actions deploy test step. (#26)
    
    * Adding GitHub Actions CI to the project.
    
    * Fixing linting issues.
    
    * Run a test Drupal 8 deploy with CI.
    
    * pip installed Ansible has a different path.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -152,7 +152,14 @@ cleanup_build_tmp_dir(){
 # - revert
 # - cleanup
 ansible_play(){
-  ANSIBLE_CMD="/usr/bin/ansible-playbook $BUILD_WORKSPACE/$TARGET_DEPLOY_PLAYBOOK"
+  # Ubuntu PPA repo installed
+  if [ -f "/usr/bin/ansible-playbook" ]; then
+    ANSIBLE_BIN="/usr/bin/ansible-playbook"
+  # or pip installed
+  else
+    ANSIBLE_BIN="/usr/local/bin/ansible-playbook"
+  fi
+  ANSIBLE_CMD="$ANSIBLE_BIN $BUILD_WORKSPACE/$TARGET_DEPLOY_PLAYBOOK"
   if [ "$DRY_RUN" = "yes" ]; then
     ANSIBLE_CMD="$ANSIBLE_CMD --check"
   fi
